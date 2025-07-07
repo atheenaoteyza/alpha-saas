37e4c223c40900150169b35d1ad2f233fec851d7
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-07-07 23:23:34 +0800

    fix: refactor logic to return Vertical columns for each week

--- utils/getAllDaysInCurrentYear.js
+++ utils/getAllDaysInCurrentYear.js
@@ -1,13 +1,34 @@
+// utils/getAllDaysInCurrentYear.js
 export default function getAllDaysInCurrentYear() {
   const year = new Date().getFullYear();
-  const startDate = new Date(year, 0, 1);
-  const endDate = new Date(year, 11, 31);
+  const start = new Date(year, 0, 1);
+  const end = new Date(year, 11, 31);
 
-  const dates = [];
-  let currentDate = new Date(startDate);
-  while (currentDate <= endDate) {
-    dates.push(new Date(currentDate).toISOString().split("T")[0]);
+  const weeks = [];
+  let currentWeek = [];
+  let currentDate = new Date(start);
+
+  // fill the first week with null if not started on Sunday
+  for (let i = 0; i < currentDate.getDay(); i++) {
+    currentWeek.push(null);
+  }
+
+  while (currentDate <= end) {
+    currentWeek.push(currentDate.toISOString().split("T")[0]);
+    if (currentWeek.length === 7) {
+      weeks.push(currentWeek);
+      currentWeek = [];
+    }
     currentDate.setDate(currentDate.getDate() + 1);
   }
-  return dates;
+
+  // fill the last week with null if not ended a 7 day week
+
+  if (currentWeek.length > 0) {
+    while (currentWeek.length < 7) {
+      currentWeek.push(null);
+    }
+    weeks.push(currentWeek);
+  }
+  return weeks;
 }
