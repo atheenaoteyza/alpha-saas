769914b5029305d4460fb725b6e90e771dab6942
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-07-12 23:51:54 +0800

    feat: positioning music player modal settings

---
+++ components/MusicPlayer.js
@@ -0,0 +1,12 @@
+export default function MusicPlayer() {
+  return (
+    <>
+      <div className="p-2 modal bg-gray-900 border border-gray-600 rounded-2xl w-[100%] flex flex-col justify-center items-center overflow-auto">
+        <div className="p-2 w-[80%]">
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </div>
+      </div>
+    </>
+  );
+}
